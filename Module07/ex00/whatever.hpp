63bedebdb2028d397861794337ce122496918e7e
hajjiyoussef-dev <hajjiyoussef016@gmail.com>
2026-04-09 22:21:04 +0100

    feat: module07

---
+++ Module07/ex00/whatever.hpp
@@ -0,0 +1,25 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+
+
+template <typename T>
+
+class whatever
+{
+    private:
+        
+    public:
+        whatever());
+        ~whatever();
+};
+
+
+
+
+
+
+
+#endif
