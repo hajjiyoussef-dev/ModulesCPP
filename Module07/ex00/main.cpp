1b3e428dfb66ad01d81b10559b916274406fa83f
hajjiyoussef-dev <hajjiyoussef016@gmail.com>
2026-04-10 19:01:52 +0100

    feat: module07 -> finish the ex00 and start the ex01

---
+++ Module07/ex00/main.cpp
@@ -0,0 +1,45 @@
+#include "whatever.hpp"
+
+
+
+int main(void){
+
+    // int a = 1;
+    // int b = 2;
+
+    // ::swap(a, b);
+
+    // std::cout << "a =  " << a << " " << "b = " << b << std::endl;
+    // std::cout << "min = " << ::min(a, b) << " max =  " << ::max(a, b) << std::endl;
+
+    // std::string c = "chaine1";
+    // std::string d = "chaine2";
+
+    // ::swap(c, d);
+    // std::cout << "c = " << c << ", d = " << d << std::endl;
+
+    // std::cout << "min = " << ::min(c, d) << std::endl;
+    // std::cout << "max = " << ::max(c, d) << std::endl;
+
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+    double e = 2.2;
+    double f = 2.3;
+
+    std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+    std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
+
+    return (0);
+
+}
--- Module07/ex00/whatever.hpp
+++ Module07/ex00/whatever.hpp
@@ -6,15 +6,24 @@
 
 
 template <typename T>
+void swap(T &a, T &b){
 
-class whatever
-{
-    private:
-        
-    public:
-        whatever());
-        ~whatever();
-};
+    T tmp;
+
+    tmp = a;
+    a  = b;
+    b = tmp;
+}
+
+template <typename T>
+T min(const T &a, const T &b){
+    return ((a > b) ? b : a);
+}
+
+template <typename T>
+T max(const T &a, const T &b){
+    return ((a > b) ? a : b );
+}
 
 
 
