cd119d875d6ba4b3b415eedebfa18a865b803fbb
hajjiyoussef-dev <hajjiyoussef016@gmail.com>
2026-04-12 02:47:23 +0100

    feat: module07 ->  start the ex02

--- Module07/ex02/Array.hpp
+++ Module07/ex02/Array.hpp
@@ -3,6 +3,46 @@
 
 #include <iostream>
 
+template <typename T>
+class Array
+{
+    private:
+        T* data;
+        unsigned int size ;
+    public:
+        Array() : data(NULL), size(0){};
+        Array(const Array& obj: size(obj.size) {
+            data = new T[size];
+            for (unsigned int i = 0; i < size; i++){
+                data[i] = obj.data[i];
+            }
 
+        }
+        ~Array(){
+
+            delete[] data;
+        }
+
+        Array(unsigned int n){
+
+            data  = new T[n];
+            for (unsigned int i = 0; i < n; i++){
+                data[i] = T();
+            }
+        }
+        Array& operator=(const Array& obj){
+            if (this != &obj){
+                delete[] data;
+                size = obj.size;
+                data = new T[size];
+                for (size_t i = 0; i < size; i++)
+                {
+                    data[i]  = obj.data[i];
+                }
+            }
+            return (*this);
+        };
+
+};
 
 #endif
