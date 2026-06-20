f945229ba9eff878a4980cefcc578151ce60316f
hajjiyoussef-dev <hajjiyoussef016@gmail.com>
2026-06-20 04:01:13 +0100

    feat: cpp 08

--- Module08/ex01/Span.cpp
+++ Module08/ex01/Span.cpp
@@ -2,10 +2,27 @@
 
 
 
-Span::Span()
-{
+Span::Span() {}
+
+Span::Span(unsigned int N) : maximum(N) {
+
 }
 
-Span::~Span()
-{
+Span::Span(const Span& obj){
+
+    *this = obj;
 }
+
+Span& Span::operator=(const Span& obj){
+
+    if (this != &obj){
+        maximum = obj.maximum;
+        nbr = obj.nbr;
+    }
+    return (*this);
+}
+
+Span::~Span() {}
+
+
+
--- Module08/ex01/Span.hpp
+++ Module08/ex01/Span.hpp
@@ -6,11 +6,16 @@
 class Span
 {
     private:
-        unsigned int maximum_n;
+        unsigned int maximum;
         std::vector<int> nbr;
     public:
         Span();
+        Span(unsigned int N);
+        Span(const Span& obj);
+        Span& operator=(const Span& obj);
         ~Span();
+
+        
 };
 
 
